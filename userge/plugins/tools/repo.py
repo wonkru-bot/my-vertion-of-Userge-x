cde2cc2d49a1048e4d191cd675372790f92dc573
MR-LEO <73919659+wonkru-bot@users.noreply.github.com>
2021-01-31 16:27:31 +0530

    Update repo.py

--- userge/plugins/tools/repo.py
+++ userge/plugins/tools/repo.py
@@ -12,5 +12,5 @@ from userge import Config, Message, userge
 @userge.on_cmd("repo", about={"header": "get repo link and details"})
 async def see_repo(message: Message):
     """see repo"""
-    output = f"• **repo** : [USERGE-X]({Config.UPSTREAM_REPO})"
+    output = f"• **repo** : [🇱 🇪 🇴𝚉 ᴬˢˢᴵˢᵀᴬᴺᵀ]({Config.UPSTREAM_REPO})"
     await message.edit(output)
